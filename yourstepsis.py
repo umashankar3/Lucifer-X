151778d4d5c3908592f16a2bdf05dd2a3b4b9116
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-06-24 20:52:50 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,6 +1,6 @@
 from os import system
-system("git clone https://gitHub.com/mdnoor786/Lion-X && cd Lion-X && python3 -m Lion")
+system("git clone https://gitHub.com/kaal0408/Lucifer-X && cd Lucifer-X && python3 -m Lucifer")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Dark_ultra_support_group")
 # Script written for stop heroku suspension
 # use with credit else gay
