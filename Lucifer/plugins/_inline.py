1ff6c60f56dd376774daf09a3d14a00c704817a7
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-11 16:40:30 +0530

    Update _inline.py

--- Lucifer/plugins/_inline.py
+++ Lucifer/plugins/_inline.py
@@ -179,7 +179,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.edit(buttons=buttons)
         else:
             reply_pop_up_alert = (
-                "Please get your own Userbot from @LionHelp , and don't use mine!"
+                "Please get your own Userbot from @LuciferHelp , and don't use mine!"
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
@@ -190,7 +190,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
         else:
             await event.edit(
-                f"This is the PM Security for {DEFAULTUSER} to keep away spammers and retards.\n\nProtected by [Lion](t.me/LionXsupport)"
+                f"This is the PM Security for {DEFAULTUSER} to keep away spammers and retards.\n\nProtected by [Lucifer](t.me/LuciferXsupport)"
             )
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"menu")))
