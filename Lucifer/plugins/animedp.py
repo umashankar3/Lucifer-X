834e482ac8ae31384b99ed27521b9d842e056ab8
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-09 15:26:50 +0530

    Update animedp.py

--- Lucifer/plugins/animedp.py
+++ Lucifer/plugins/animedp.py
@@ -1,4 +1,4 @@
-# Made By @LionHelp Keep Credits If You Are Goanna Kang This Lol
+# Made By @LuciferXsupport Keep Credits If You Are Goanna Kang This Lol
 # And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
 # Usage .actressdp I'm Not Responsible For Any Ban caused By This
 
