2599f4fad73125ce3f8c1541b786f18eb5b207dc
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-09 15:33:12 +0530

    Update app.py

--- Lucifer/plugins/app.py
+++ Lucifer/plugins/app.py
@@ -74,7 +74,7 @@ async def apk(event):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += f"\n\n===> Lion <==="
+        app_details += f"\n\n===> Lucifer <==="
         await event.edit(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await event.edit("No result found in search. Please enter **Valid app name**")
