dcabb59fae195ba858f2062e7fa480272d0be37a
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-19 22:18:03 +0530

    Update ping.py

--- Lucifer/plugins/ping.py
+++ Lucifer/plugins/ping.py
@@ -53,7 +53,7 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
     await x.edit(
-        f"MY MASTER\nI AM ON\n\n✘ **ριиg** : `{ms}`\n✘ **υρтιмє** : `{uptime}`\n✘ **𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑** : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© 𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃"
+        f"[https://telegra.ph/file/42a423c45e4146cf8a94c.mp4]\n\n✘ **ριиg** : `{ms}`\n✘ **υρтιмє** : `{uptime}`\n✘ **𝐌𝐘 𝐏𝐄𝐑𝐎 𝐌𝐀𝐒𝐓𝐄𝐑** : [{DEFAULTUSER}](tg://user?id={OWNER_ID})\n\n© 𝙻ucifer 𝚇 𝚄𝚂𝙴𝚁𝙱𝙾𝚃"
     )
 
 
