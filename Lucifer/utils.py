cc24e5ad2af401d1d176ce9310a3a9f9ffebb35a
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-20 20:58:07 +0530

    Update utils.py

--- Lucifer/utils.py
+++ Lucifer/utils.py
@@ -422,7 +422,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[Here](https://t.me/lightning_support_group)"
+            link = "[Here](https://t.me/Lucifer_support_group)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
@@ -433,7 +433,7 @@ def errors_handler(func):
             ftext += "\nyou may not report this error if you've"
             ftext += "\nany confidential data here, no one will see your data\n\n"
 
-            ftext += "--------BEGIN Lightning USERBOT TRACEBACK LOG--------"
+            ftext += "--------BEGIN Lucifer USERBOT TRACEBACK LOG--------"
             ftext += "\nDate: " + date
             ftext += "\nGroup ID: " + str(errors.chat_id)
             ftext += "\nSender ID: " + str(errors.sender_id)
