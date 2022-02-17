39f840a1de818ae15d74bc639e2aa8d37b2aef26
Umashankar <91821821+umashankar3@users.noreply.github.com>
2022-02-17 20:43:25 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,11 +1,11 @@
 from os import system
 
 system(
-    "git clone https://gitHub.com/kaal0408/Lucifer-X && cd Lucifer-X && python3 -m Lucifer"
+    "git clone https://gitHub.com/Umashankar3/Lucifer-X && cd Lucifer-X && python3 -m Lucifer"
 )
 # if got error
 print(
-    " Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group"
+    " Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @shayari_jok"
 )
 # Script written for stop heroku suspension
 # use with credit else gay
